c8f4d5d87086a270766705629e922aa366e762d3
Akeret, Joel <joel.akeret@gmail.com>
2019-05-15 09:47:45 +0200

    ranking and calling logic

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python for Data Science Practitioners Course Material
+
+## Portfolio and Instrument risk assessment package
---
+++ ranking.py
@@ -0,0 +1,39 @@
+import math
+import numpy as np
+import pandas as pd
+
+
+def compute_instrument_ranks(instruments, ch_cl=False):
+    rating = 0
+
+    #check if swiss client
+    if ch_cl:
+        for instrument in instruments:
+            if not instrument["ignore"]:
+                if not instrument["expired"]:
+
+                    instr_curr = instrument["currency"]
+
+                    if instr_curr == 'USD':
+                        rating += 7
+                    elif instr_curr == 'EUR':
+                        rating += 6
+                    elif instr_curr == 'GBP':
+                        rating += 5
+                    else:
+                        rating += 1
+    else:
+        rating = len(instruments) * 100
+
+    return rating
+
+
+def is_restricted(portfolio):
+    result = (portfolio["mandator"] == "INTL" or portfolio["mandator"] == "EU") and portfolio[
+        "currency"] != "CHF"
+
+    # if not restricted then set the prechecked flag
+    if result == False:
+        portfolio["prechecked"] = True
+
+    return result
