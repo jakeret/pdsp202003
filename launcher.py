fc1abf3b7a03acf6522bb79d5691ecc6ef060937
Akeret, Joel <joel.akeret@gmail.com>
2019-05-16 09:15:46 +0200

    adds tests for the eligbility verification

--- launcher.py
+++ launcher.py
@@ -1,55 +1,7 @@
+from pdsp2019 import eligibility
 from pdsp2019.ranking import *
 # Portfolio and Instrument risk assessment package
 
 ### Use the portfolio and instrument ranking package to verify if the customer is eligible for new investments
 
-#### Get the portfolios and instruments to check
-
-intl_pf = {
-    "mandator": "INTL",
-    "currency": "USD"
-}
-
-eu_pf = {
-    "mandator": "INTL",
-    "currency": "USD"
-}
-
-eu_pf_with_chf = {
-    "mandator": "EU",
-    "currency": "CHF"
-}
-
-ch_pf = {
-    "mandator": "CH",
-    "currency": "USD"
-}
-
-
-#### Check if the portfolios are restricted by regulatory definition
-print("International mandator portfolio restricted:", is_restricted(intl_pf))
-print("European mandator portfolio restricted:", is_restricted(eu_pf))
-print("European mandator portfolio with swiss currency restricted:", is_restricted(eu_pf_with_chf))
-print("Swiss mandator portfolio restricted:", is_restricted(ch_pf))
-
-#### Use the portfolios that are now prechecked and not restricted to test for their instruments
-print("European mandator portfolio with swiss currency:", eu_pf_with_chf)
-print("Swiss mandator portfolio:", ch_pf)
-
-eu_pf_with_chf_instr = [
-    {"currency": "CHF", "ignore":False, "expired":False},
-    {"currency": "EUR", "ignore":True, "expired":False},
-]
-
-ch_pf_instr = [
-    {"currency": "CHF", "ignore":False, "expired":False},
-    {"currency": "EUR", "ignore":False, "expired":False},
-]
-
-#### Instrument ranking for non swiss client
-ranking = compute_instrument_rank_for_non_swiss_clients(eu_pf_with_chf_instr)
-print("Ranking of EU portfolio is", ranking, "and to high for new investments")
-
-#### Instrument ranking for swiss client
-ranking = compute_instrument_ranks_for_swiss_clients(ch_pf_instr)
-print("Ranking of CH portfolio is", ranking, "and eligible for new investments")
+eligibility.verify_eligibility()
